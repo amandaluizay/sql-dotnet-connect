c3d40b094564831e07a064d229983a7b88ccbc4e
amanda <luiza.amandasilvah@gmail.com>
2022-08-12 14:09:45 -0300

    modulo 3

---
+++ Models/Course.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BaltaDataAccess.Models{
+
+    public class Course{
+        public Guid Id{get; set;}
+        public string Title {get; set;}
+
+        public Course course { get; set; }
+    }
+}
